864fc08c865160d379a8213ce2642b44142fa32a
Sazzad <sazadulbg130@gmail.com>
2022-02-13 01:52:07 +0600

    remove commentout

--- resources/views/chef/getchef.blade.php
+++ resources/views/chef/getchef.blade.php
@@ -3,9 +3,9 @@
 <h3>Name: {{$n}}</h3>
 <h3>Location: {{$loc}}</h3>
 
-<!-- @for($i=0; $i<1; $i++ )
+@for($i=0; $i<1; $i++ )
     <p> {{$food[$i]}} </p>
-@endfor -->
+@endfor
 
 <ul>
     @foreach($food as $f)
